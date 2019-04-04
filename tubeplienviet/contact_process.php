6cbb905d6bb6b2f83a5cf3d91bef0031c4eeae19
huuhieung <34087343+huuhieung@users.noreply.github.com>
2019-04-04 22:26:14 +0700

    ssd

--- tubeplienviet/contact_process.php
+++ tubeplienviet/contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "huuhieung90@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
